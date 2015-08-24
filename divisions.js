e43a0a3775345d7064a0142a6df178dfc8e234ed
Kenny Howard <ken.chad.howard@gmail.com>
2015-08-24 16:38:36 -0600

    shuffle array works

--- divisions.js
+++ divisions.js
@@ -1,5 +1,13 @@
 var leagueArray = ["Evan", "Kyle", "Kenny", "Simon", "Sean", "Austin", "Micah", "Mat", "Calie", "Andrew"];
 
 function shuffle(array) {
-
+  var newArray = array;
+  for (var i = newArray.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = newArray[i];
+    newArray[i] = newArray[j];
+    newArray[j] = temp
+  };
+  return array;
 };
+shuffle(leagueArray);
